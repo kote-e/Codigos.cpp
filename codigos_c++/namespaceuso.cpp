19f8855c241771faa731e7386c14c0b955bd220b
kote-e <mjosesanm@gmail.com>
2025-08-16 16:58:24 -0400

    actualizar

---
+++ codigos_c++/namespaceuso.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+namespace miespaciodenombres{  // Namespace declaration, funciones de matemáticas
+    int suma(int a, int b) {
+        return a + b;
+    }
+    int resta(int a, int b) {
+        return a - b;
+    }
+    int multiplicacion(int a, int b) {
+        return a * b;
+    }
+    int division(int a, int b) {
+        if (b == 0) {
+            cout << "Error: Division by zero!" << endl;
+            return 0; 
+        }
+        return a / b;
+    }
+}
+
+int main() {
+    using namespace miespaciodenombres;  // Using the namespace
+    int a = 10, b = 5;
+    
+    cout << "Suma: " << suma(a, b) << endl;
+    cout << "Resta: " << resta(a, b) << endl;
+    cout << "Multiplicación: " << multiplicacion(a, b) << endl;
+    cout << "División: " << division(a, b) << endl;
+
+    return 0;
+}
