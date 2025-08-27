2472f18d735134ec00ef16cd3eeef4bb7027f535
kote-e <mjosesanm@gmail.com>
2025-08-26 23:37:18 -0400

    actualizar

---
+++ trabajos_universidad/ejercicios/2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cout << "ingresa dos numeros: ";
+    cin >> a >> b;
+    cout << "la suma es: " << a+b << endl;
+    return 0;
+}
---
+++ trabajos_universidad/ejercicios/guia_1/1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string nombre;
+    cout << "ingresa tu nombre: ";
+    cin >> nombre;
+    cout << "Hola " << nombre << " Bienvenido a C++" << endl;
+
+    return 0;
+}
