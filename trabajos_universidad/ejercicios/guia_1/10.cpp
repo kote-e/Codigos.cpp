cc3e16b1f46dcdbadaa2afc76f64a698a52fe8d1
kote-e <mjosesanm@gmail.com>
2025-09-06 18:47:11 -0400

    actualizar

--- trabajos_universidad/ejercicios/guia_1/10.cpp
+++ trabajos_universidad/ejercicios/guia_1/10.cpp
@@ -2,7 +2,25 @@
 using namespace std;
 
 main(){
-    int numero;
-    cout << "Ingrese un numero: ";
-    
+    int numero, cantidad, cantidadu=0, positivos=0, negativos=0, ceros=0;
+    cout << "Ingrese la cantidad de numeros a procesar: ";
+    cin >> cantidad;
+
+    do{
+        cout << "Ingrese un numero: ";
+        cin >> numero;
+        if(numero > 0){
+            positivos++;
+        } else if(numero < 0){
+            negativos++;
+        } else {
+            ceros++;
+        }
+        cantidadu++;
+    }
+    while(cantidadu < cantidad);
+    cout << "Cantidad de positivos: " << positivos << endl;
+    cout << "Cantidad de negativos: " << negativos << endl; 
+    cout << "Cantidad de ceros: " << ceros << endl;
+    return 0;
 }
