797b446f56663c5733fb33b8ca9388f187110d74
kote-e <mjosesanm@gmail.com>
2025-09-08 22:01:58 -0300

    actualizar

--- trabajos_universidad/ejercicios/guia_2/4.cpp
+++ trabajos_universidad/ejercicios/guia_2/4.cpp
@@ -9,11 +9,17 @@ int cuadrado(int numero){
         return cuadrado(numero - 1) + (1+ ((numero -1) *2));
 
     } 
-    // 4**2= 16   1+3+5+7=16
-    // 3**2= 9    1+3+5=9
-
-
 }
+// 4**2 = 16 = 7 + 5 + 3 + 1
+// 3**2 = 9 = 5 + 3 + 1
+// 2**2 = 4 = 3 + 1
+// 1**2 = 1 = 1
+
+// cuadrado(4) = cuadrado(3) + 7 = 16
+// cuadrado(3) = cuadrado(2) + 5 = 9
+// cuadrado(2) = cuadrado(1) + 3 = 4
+// cuadrado(1) = 1
+
 
 int main(){
     int n;
