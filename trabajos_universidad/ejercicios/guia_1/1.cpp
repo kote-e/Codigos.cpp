69a223dc2a7fdb3770ff99d287b92d95c3b4dd99
kote-e <mjosesanm@gmail.com>
2025-09-08 23:00:35 -0300

    actualizar

--- trabajos_universidad/ejercicios/guia_1/1.cpp
+++ trabajos_universidad/ejercicios/guia_1/1.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
     string nombre;
     cout << "ingresa tu nombre: ";
-    cin >> nombre;
+    getline(cin, nombre);
     cout << "Hola " << nombre << " Bienvenido a C++" << endl;
 
     return 0;
--- trabajos_universidad/ejercicios/guia_1/15.cpp
+++ trabajos_universidad/ejercicios/guia_1/15.cpp
@@ -2,14 +2,18 @@
 using namespace std;
 
 int main(){
-    string contraseña ="contrasena", contraseña_ingresada;
+    string contraseña ="contrase", contraseña_ingresada;
     do{
         cout << "ingrese la contraseña: ";
         cin >> contraseña_ingresada;
-        if(contraseña_ingresada != contraseña){
+
+        if (contraseña_ingresada.length()!= 8){
+            cout << "la contraseña debe tener 8 caracteres" << endl;
+        }
+        else if(contraseña_ingresada != contraseña){
             cout << "contraseña incorrecta, intente de nuevo" << endl;
         }
-    }while(contraseña_ingresada != contraseña);
+    }while(contraseña_ingresada.length()!= 8 && contraseña_ingresada != contraseña);
     cout << "contraseña correcta" << endl;
     
 }
