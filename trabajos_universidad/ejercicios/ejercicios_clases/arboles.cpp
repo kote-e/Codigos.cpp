dc317d47ad7ee66df8540a91cf71e4fc4230edfb
kote-e <mjosesanm@gmail.com>
2025-11-01 16:50:30 -0300

    actualizar

---
+++ trabajos_universidad/ejercicios/ejercicios_clases/arboles.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+using namespace std;
+
+struct nodo {
+    int dato;
+    nodo* izq;
+    nodo* der;
+};
+
+nodo* vrearnodo(int dato){
+    nodo* nuevonodo= new nodo();
+    nuevonodo->dato=dato;
+    nuevonodo->izq=nullptr;
+    nuevonodo->der=nullptr;
+    return nuevonodo;
+};
+
+void preorden(nodo* raiz){
+    if(raiz!=nullptr){
+        cout<<raiz->dato<<" ";
+        preorden(raiz->izq);
+        preorden(raiz->der);
+    }
+}
+
+void inorden(nodo* raiz){
+    if(raiz!=nullptr){
+        inorden(raiz->izq);
+        cout<<raiz->dato<<" ";
+        inorden(raiz->der);
+    }
+}
+
+void postorden(nodo* raiz){
+    if (raiz!=nullptr){
+        postorden(raiz->izq);
+        postorden(raiz->der);
+        cout<<raiz->dato<<" ";
+    }
+}
+
+void max( nodo* raiz){
+    if (raiz!=nullptr){
+        max(raiz->der);
+        cout<<raiz->dato<<" ";
+        max(raiz->izq);
+        
+    }
+}
+
+
+int main(){
+    nodo* raiz=vrearnodo(1);
+    raiz->izq=vrearnodo(2);
+    raiz->der=vrearnodo(3);
+    raiz->izq->izq=vrearnodo(4);
+    raiz->izq->der=vrearnodo(5);
+
+    cout<<"Recorrido en preorden: ";
+    preorden(raiz);
+    cout<<endl;
+
+    cout<<"Recorrido en inorden: ";
+    inorden(raiz);
+    cout<<endl;
+
+    cout<<"Recorrido en postorden: ";
+    postorden(raiz);
+    cout<<endl;
+
+    cout<<"Elementos del arbol de mayor a menor: ";
+    max(raiz);
+    cout<<endl;
+
+    return 0;
+}
